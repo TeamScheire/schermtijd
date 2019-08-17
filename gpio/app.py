62671049934de50c575f9610305c16e6ee4aa305
Kurt Beheydt <kurt@postduif.be>
2019-08-17 17:27:11 +0200

    status van gsmslots op oled scherm + debugmode voor oled scherm met systeminfo

--- gpio/app.py
+++ gpio/app.py
@@ -1,14 +1,57 @@
 #!/usr/bin/env python
 
-import sched, time, requests
+import sched, time, requests, subprocess
 import RPi.GPIO as GPIO
 from Adafruit_LED_Backpack import SevenSegment
 from Adafruit_LED_Backpack import Matrix8x8
+import Adafruit_GPIO.SPI as SPI
+import Adafruit_SSD1306
 
+from PIL import Image
+from PIL import ImageDraw
+from PIL import ImageFont
+
+# Raspberry Pi pin configuration:
+RST = None     # on the PiOLED this pin isnt used
+# Note the following are only used with SPI:
+DC = 23
+SPI_PORT = 0
+SPI_DEVICE = 0
+
+# 128x32 display with hardware I2C:
+disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+disp.begin()
+disp.clear()
+disp.display()
+# Create blank image for drawing.
+# Make sure to create image with mode '1' for 1-bit color.
+width = disp.width
+height = disp.height
+image = Image.new('1', (width, height))
+
+# Get drawing object to draw on image.
+draw = ImageDraw.Draw(image)
+
+# Draw a black filled box to clear the image.
+draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+# Draw some shapes.
+# First define some constants to allow easy resizing of shapes.
+padding = -2
+top = padding
+bottom = height-padding
+# Move left to right keeping track of the current x position for drawing shapes.
+x = 0
+
+font = ImageFont.truetype('Retron2000.ttf', 12)
+fontDebug = ImageFont.load_default()
+
+# 8X8 led matrix
 matrixLeft = Matrix8x8.Matrix8x8()
 matrixLeft.begin()
 matrixRight = Matrix8x8.Matrix8x8(address=0x72)
 matrixRight.begin()
+# 7-segment display
 display = SevenSegment.SevenSegment(address=0x71, busnum=1)
 display.begin()
 
@@ -16,10 +59,10 @@ gsmSlots = [
 	# buttonpin, ledpin, toestelnumber
 	[4, 18, 1],
 	[17, 23, 2],
-	#[27, 24, 3],
-	#[22, 15, 4],
-	#[6, 12, 5],
-	#[13, 16, 6]
+	[27, 24, 3],
+	[22, 15, 4],
+	[6, 12, 5],
+	[13, 16, 6]
 ]
 
 doosButton = [19, 20] # gpio poort waarop de knop van het deksel van de doos aangesloten is, status led
@@ -31,6 +74,7 @@ activeButtons = []
 points = 0
 
 apiurl = 'http://localhost:3000/api/'
+debugMode = 0
 
 def setup():
 	GPIO.setmode(GPIO.BCM)
@@ -122,10 +166,15 @@ def calculatePoints():
 		resetPoints()
 
 def handleButton(buttonPin, ledPin, toestelNumber):
-	global activeButtons
+	global activeButtons, debugMode
+	time.sleep(.1)
 	buttonStatus = GPIO.input(buttonPin)
 	print 'gedrukt op pin: ', buttonPin, ' - status: ', buttonStatus
 	GPIO.output(ledPin, buttonStatus)
+	if (statusDoosDeksel == 1):
+		#debugmode toggle: als er een knop van de gsms wijzigt als de doos dicht is.
+		debugMode = 1 - debugMode
+
 	if (buttonStatus):
 		if toestelNumber not in activeButtons:
 			activeButtons.append(toestelNumber)
@@ -170,9 +219,47 @@ def handleActicviteitButton(buttonPin, ledPin):
 		url = apiurl + 'activiteit/random/{}'.format(activeButtons)
 		# TODO printen in async, met op het einde statusPrinting = 0 en led terug uit
 
+def drawOled():
+	global draw, disp, width, height, top, bottom, debugMode
+	# Draw a black filled box to clear the image.
+	draw.rectangle((0,0,width,height), outline=0, fill=0)
+
+	if (debugMode == 1):
+		# Shell scripts for system monitoring from here : https://unix.stackexchange.com/questions/119126/command-to-display-memory-usage-disk-usage-and-cpu-load
+		cmd = "hostname -I | cut -d\' \' -f1"
+		IP = subprocess.check_output(cmd, shell = True )
+		cmd = "top -bn1 | grep load | awk '{printf \"CPU Load: %.2f\", $(NF-2)}'"
+		CPU = subprocess.check_output(cmd, shell = True )
+		cmd = "free -m | awk 'NR==2{printf \"Mem: %s/%sMB %.2f%%\", $3,$2,$3*100/$2 }'"
+		MemUsage = subprocess.check_output(cmd, shell = True )
+		cmd = "df -h | awk '$NF==\"/\"{printf \"Disk: %d/%dGB %s\", $3,$2,$5}'"
+		Disk = subprocess.check_output(cmd, shell = True )
+
+		# Write four lines of text.
+		draw.text((x, top),       "IP: " + str(IP),  font=fontDebug, fill=255)
+		draw.text((x, top+8),     str(CPU), font=fontDebug, fill=255)
+		draw.text((x, top+16),    str(MemUsage),  font=fontDebug, fill=255)
+		draw.text((x, top+25),    str(Disk),  font=fontDebug, fill=255)
+
+	else:
+		presentLine = '  '
+		for slot in gsmSlots:
+			if slot[2] in activeButtons:
+				presentLine += ' x |'
+			else:
+				presentLine += ' o |'
+		presentLine = presentLine[:-1]
+		draw.text((x, top), "   1 | 2 | 3 | 4 | 5 | 6",  font=font, fill=255)
+		draw.text((x, top+17), presentLine,  font=font, fill=255)
+
+	# Display image.
+	disp.image(image)
+	disp.display()
+
 def loop():
 	while True:
 		calculatePoints()
+		drawOled()
 		time.sleep(1)
 
 def destroy():
@@ -188,6 +275,9 @@ def destroy():
 		matrixLeft.write_display()
 		matrixRight.clear()
 		matrixRight.write_display()
+		draw.rectangle((0,0,width,height), outline=0, fill=0)
+		disp.image(image)
+		disp.display()
 		GPIO.cleanup()
 	except:
 		print 'I2C error'
