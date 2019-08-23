d229d7e26c82cf0c105c4de83e4c941f37069fc7
Kurt Beheydt <kurt@postduif.be>
2019-08-23 20:28:40 +0200

    alle slots aangesloten + bugfixes

--- gpio/app.py
+++ gpio/app.py
@@ -13,6 +13,7 @@ from PIL import ImageDraw
 from PIL import ImageFont
 
 displayMode = True # debugmode for no displays
+oledMode = False
 
 # Raspberry Pi pin configuration:
 RST = None     # on the PiOLED this pin isnt used
@@ -23,34 +24,36 @@ SPI_DEVICE = 0
 
 try:
 	print('testing I2C')
-	# 128x32 display with hardware I2C:
-	disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
-	disp.begin()
-	disp.clear()
-	disp.display()
-	# Create blank image for drawing.
-	# Make sure to create image with mode '1' for 1-bit color.
-	width = disp.width
-	height = disp.height
-	image = Image.new('1', (width, height))
+	
+	if (oledMode):
+		# 128x32 display with hardware I2C:
+		disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
+		disp.begin()
+		disp.clear()
+		disp.display()
+		# Create blank image for drawing.
+		# Make sure to create image with mode '1' for 1-bit color.
+		width = disp.width
+		height = disp.height
+		image = Image.new('1', (width, height))
 
-	# Get drawing object to draw on image.
-	draw = ImageDraw.Draw(image)
+		# Get drawing object to draw on image.
+		draw = ImageDraw.Draw(image)
 
-	# Draw a black filled box to clear the image.
-	draw.rectangle((0,0,width,height), outline=0, fill=0)
+		# Draw a black filled box to clear the image.
+		draw.rectangle((0,0,width,height), outline=0, fill=0)
 
-	# Draw some shapes.
-	# First define some constants to allow easy resizing of shapes.
-	padding = -2
-	top = padding
-	bottom = height-padding
-	# Move left to right keeping track of the current x position for drawing shapes.
-	x = 0
+		# Draw some shapes.
+		# First define some constants to allow easy resizing of shapes.
+		padding = -2
+		top = padding
+		bottom = height-padding
+		# Move left to right keeping track of the current x position for drawing shapes.
+		x = 0
 
-	font = ImageFont.truetype('./Retron2000.ttf', 12)
-	fontDebug = ImageFont.load_default()
-	print('Adafruit_SSD1306 display loaded')
+		font = ImageFont.truetype('./Retron2000.ttf', 12)
+		fontDebug = ImageFont.load_default()
+		print('Adafruit_SSD1306 display loaded')
 
 	# 8X8 led matrix
 	matrixRight = Matrix8x8.Matrix8x8()
@@ -64,8 +67,9 @@ try:
 	display.begin()
 	print('SevenSegment loaded')
 
-except:
+except Exception as e:
 	print('error loading I2C')
+	print(e)
 	displayMode = False
 
 gsmSlots = [
@@ -73,7 +77,7 @@ gsmSlots = [
 	[4, 18, 1],
 	[17, 23, 2],
 	[27, 24, 3],
-	[22, 15, 4],
+	[22, 25, 4],
 	[6, 12, 5],
 	[13, 16, 6]
 ]
@@ -102,10 +106,10 @@ def setup():
 
 	GPIO.add_event_detect(gsmSlots[0][0], GPIO.BOTH, callback = lambda x: handleButton(gsmSlots[0][0], gsmSlots[0][1], gsmSlots[0][2]))
 	GPIO.add_event_detect(gsmSlots[1][0], GPIO.BOTH, callback = lambda x: handleButton(gsmSlots[1][0], gsmSlots[1][1], gsmSlots[1][2]))
-	#GPIO.add_event_detect(gsmSlots[2][0], GPIO.BOTH, callback = lambda x: handleButton(gsmSlots[2][0], gsmSlots[2][1], gsmSlots[2][2]))
-	#GPIO.add_event_detect(gsmSlots[3][0], GPIO.BOTH, callback = lambda x: handleButton(gsmSlots[3][0], gsmSlots[3][1], gsmSlots[3][2]))
-	#GPIO.add_event_detect(gsmSlots[4][0], GPIO.BOTH, callback = lambda x: handleButton(gsmSlots[4][0], gsmSlots[4][1], gsmSlots[4][2]))
-	#GPIO.add_event_detect(gsmSlots[5][0], GPIO.BOTH, callback = lambda x: handleButton(gsmSlots[5][0], gsmSlots[5][1], gsmSlots[5][2]))
+	GPIO.add_event_detect(gsmSlots[2][0], GPIO.BOTH, callback = lambda x: handleButton(gsmSlots[2][0], gsmSlots[2][1], gsmSlots[2][2]))
+	GPIO.add_event_detect(gsmSlots[3][0], GPIO.BOTH, callback = lambda x: handleButton(gsmSlots[3][0], gsmSlots[3][1], gsmSlots[3][2]))
+	GPIO.add_event_detect(gsmSlots[4][0], GPIO.BOTH, callback = lambda x: handleButton(gsmSlots[4][0], gsmSlots[4][1], gsmSlots[4][2]))
+	GPIO.add_event_detect(gsmSlots[5][0], GPIO.BOTH, callback = lambda x: handleButton(gsmSlots[5][0], gsmSlots[5][1], gsmSlots[5][2]))
 
 	GPIO.setup(doosButton[0], GPIO.IN)
 	GPIO.setup(doosButton[1], GPIO.OUT)
@@ -217,7 +221,7 @@ def handleButton(buttonPin, ledPin, toestelNumber):
 	global activeButtons, debugMode
 	time.sleep(.1)
 	buttonStatus = GPIO.input(buttonPin)
-	print('gedrukt op pin: ', buttonPin, ' - status: ', buttonStatus)
+	print('gedrukt op pin: ' + buttonPin + ' - status: ' + buttonStatus)
 	GPIO.output(ledPin, buttonStatus)
 	if (statusDoosDeksel == 1):
 		#debugmode toggle: als er een knop van de gsms wijzigt als de doos dicht is.
@@ -246,7 +250,7 @@ def handledoosButton(buttonPin, ledPin):
 	time.sleep(.1)
 	buttonStatus = GPIO.input(buttonPin)
 	GPIO.output(ledPin, buttonStatus)
-	print('deksel van de doos is veranderd: ', buttonPin, ' - status: ', buttonStatus)
+	print('deksel van de doos is veranderd: ' + buttonPin + ' - status: ' + buttonStatus)
 	# als van dicht naar open
 	if ((statusDoosDeksel == 1) and (buttonStatus == 0)):
 		print('Doos is open gegaan')
@@ -277,12 +281,11 @@ def handleActiviteitButton(buttonPin, ledPin):
 	global statusPrinting, activeButtons, statusDoosDeksel
 	time.sleep(.1)
 	buttonStatus = GPIO.input(buttonPin)
-	print('Activiteitknop is ingedrukt: ', buttonPin, ' - status: ', buttonStatus)
+	print('Activiteitknop is ingedrukt: ' + buttonPin + ' - status: ' + buttonStatus)
 	if ((statusPrinting == 0) and (buttonStatus == 1) and (len(activeButtons) >= 1) and (statusDoosDeksel == 1)):
 		print('action to print')
 		statusPrinting = 1
 		GPIO.output(ledPin, statusPrinting)
-		#printActiviteit(activeButtons, ledPin)
 		printThread = Thread(target=printActiviteit, args=(activeButtons, ledPin,))
 		printThread.start()
 
@@ -327,7 +330,8 @@ def drawOled():
 def loop():
 	while True:
 		calculatePoints()
-		drawOled()
+		if (oledMode):
+			drawOled()
 		time.sleep(1)
 
 def destroy():
@@ -342,6 +346,7 @@ def destroy():
 		if (displayMode):
 			display.clear()
 			display.write_display()
+		if (oledMode):
 			draw.rectangle((0,0,width,height), outline=0, fill=0)
 			disp.image(image)
 			disp.display()
