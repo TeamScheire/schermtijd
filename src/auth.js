534040140aa5d324377b08dca09dfe65a743b72f
Kurt Beheydt <kurt@postduif.be>
2019-09-19 23:14:39 +0200

    fixed absolute path for env file

--- src/auth.js
+++ src/auth.js
@@ -1,9 +1,14 @@
 const auth = require('basic-auth');
-require('dotenv').config();
+let path = require("path");
+require('dotenv').config({
+    path: path.join(__dirname, ".env")
+});
 
 module.exports = function (request, response, next) {
     var user = auth(request);
     if (!user || process.env.adminPass !== user.pass) {
+        console.log('correct password: ' + process.env.adminPass);
+        if (user) console.log('user password: ' + user.pass);
         response.set('WWW-Authenticate', 'Basic realm="Log in"');
         return response.status(401).send();
     }
